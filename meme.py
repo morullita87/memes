025dba540d7582d3687bfcfec11da06479376246
morullita87 <124706089+morullita87@users.noreply.github.com>
2023-03-14 17:44:57 +0100

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,1 +1,13 @@
 
+import streamlit as st
+import requests
+import json
+
+if st.button("Generar meme"):
+	url= "http://meme-api.com/gimme/wholesomememes"
+	respuesta = requests.get(url)
+	data = json.loads(respuesta.text)
+	meme = data["url"]
+	st.title(data["title"])
+	
+	st.markdown("![Una imagen graciosa](+meme+")")
